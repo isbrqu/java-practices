8d3aaec4afd2ea7ebeb50a9a6e73aa95f03da1ac
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-01-31 21:57:17 -0300

    agrega warning-unchecked

---
+++ warning-unchecked/src/Main.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+@SuppressWarnings("rawtypes")
+class Main {
+
+  public Comparable hola;
+
+  public static void main(String[] args) {
+    System.out.println("Enter two numbers");
+  }
+
+}
