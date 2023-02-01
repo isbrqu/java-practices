c279411cb5a2f3eb39c50c3967dcf1e89e5b4416
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-01 09:09:09 -0300

    hace más eficiente el algoritmo

--- variations/src/Main.java
+++ variations/src/Main.java
@@ -4,22 +4,27 @@ class Main {
 
   public static void main(String[] args) {
     LinkedList<Integer> contador = new LinkedList<Integer>();
-    int length = 3;
+    int length = 4;
     int base = 3;
-    contador.add(0);
+    int i = 0;
+    int v = 0;
+    boolean flag = true;
     while (contador.size() <= length) {
-      for (int i = 1; i <= base; i++) {
-        System.out.println(contador);
-        contador.set(0, i);
-      }
-      for (int i = 0; i < contador.size(); i++) {
-        if (contador.get(i) == base) {
+      if (i == contador.size()) {
+        contador.add(0);
+        if (contador.size() <= length)
+          System.out.println(contador);
+        i = 0;
+      } else {
+        v = contador.get(i) + 1;
+        if (v < base) {
+          contador.set(i, v);
+          System.out.println(contador);
+          if (i > 0)
+            i = 0;
+        } else {
           contador.set(i, 0);
-          int next = i + 1;
-          if (next == contador.size())
-            contador.add(0);
-          else
-            contador.set(next, contador.get(next) + 1);
+          i++;
         }
       }
     }
