539ca80890598f9f7d2f668906f1af0e96852528
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-08 10:41:57 -0300

    generar los variaciones de un array como alfabeto

--- variations/src/Main.java
+++ variations/src/Main.java
@@ -1,8 +1,20 @@
 import java.util.LinkedList;
+import java.util.Arrays;
 
 class Main {
 
   public static void main(String[] args) {
+    String[] alphabet = {
+      "1", "0"
+    };
+    Object[] variations = variations(alphabet);
+    for (Object variation : variations) {
+      String[] v = (String[]) variation;
+      System.out.println(Arrays.toString(v));
+    }
+  }
+
+  public static void variations() {
     LinkedList<Integer> contador = new LinkedList<Integer>();
     int length = 4;
     int base = 3;
@@ -27,6 +39,43 @@ class Main {
         }
       }
     }
+  }
+  
+  public static Object[] variations(String[] alphabet) {
+    LinkedList<Integer> contador = new LinkedList<Integer>();
+    LinkedList<String[]> result = new LinkedList<String[]>();
+    String[] variation;
+    int length = 4;
+    int base = alphabet.length;
+    int i = 0;
+    int v = 0;
+    while (contador.size() <= length) {
+      if (i == contador.size()) {
+        contador.add(0);
+        if (contador.size() <= length) {
+          variation = new String[contador.size()];
+          for (int j = 0; j < contador.size(); j++)
+            variation[j] = alphabet[contador.get(j)];
+          result.add(variation);
+        }
+        i = 0;
+      } else {
+        v = contador.get(i) + 1;
+        if (v < base) {
+          contador.set(i, v);
+          variation = new String[contador.size()];
+          for (int j = 0; j < contador.size(); j++)
+            variation[j] = alphabet[contador.get(j)];
+          result.add(variation);
+          if (i > 0)
+            i = 0;
+        } else {
+          contador.set(i, 0);
+          i++;
+        }
+      }
+    }
+    return result.toArray();
   }
 
 }
