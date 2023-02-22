a7a246612580377d3a32d53f35c87ec4a6d7eabd
Isaac Bruno Quiroga <isbrqu@gmail.com>
2023-02-22 13:55:59 -0300

    add: test with strings

---
+++ string/src/Main.java
@@ -0,0 +1,12 @@
+import java.lang.Math;
+
+class Main {
+
+  public static void main(String[] args) {
+    String str = "hola que tal";
+    for (int i = 0; i < str.length(); i++) {
+      System.out.println(Character.toString(str.charAt(i)));
+    }
+  }
+
+}
